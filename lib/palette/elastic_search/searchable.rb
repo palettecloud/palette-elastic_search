837cea80126107cc5e0850fbc59cdb0d50714251
Pinoshino <pino@machikoe.com>
2018-05-17 15:42:34 +0900

    indexの共通処理の修正

--- lib/palette/elastic_search/searchable.rb
+++ lib/palette/elastic_search/searchable.rb
@@ -30,15 +30,14 @@ module Palette
           current_indices.first
         end
 
-        def indexing_process
-          self.__elasticsearch__.client.indices.create index: new_index_name,
+        def indexing_process(index_name)
+          self.__elasticsearch__.client.indices.create index: index_name,
                                                        body: {
                                                          settings: self.settings.to_hash,
                                                          mappings: self.mappings.to_hash
                                                        }
-
           process_started_at = Time.current
-          self.__elasticsearch__.import(index: new_index_name)
+          self.__elasticsearch__.import(index: index_name)
           process_end_at = Time.current
 
           # @note for new records generated while indexing
@@ -48,7 +47,7 @@ module Palette
               previous_end_at = process_end_at
               process_started_at = Time.current
               # @see https://github.com/elastic/elasticsearch-rails/blob/master/elasticsearch-model/lib/elasticsearch/model/importing.rb
-              self.__elasticsearch__.import(index: new_index_name) query: -> { where(updated_at: previous_started_at..previous_end_at) }
+              self.__elasticsearch__.import(index: index_name, query: -> { where(updated_at: previous_started_at..previous_end_at) })
               process_end_at = Time.current
               break if self.where(updated_at: process_started_at..process_end_at).empty?
             end
@@ -57,8 +56,7 @@ module Palette
 
         def create_index!
           new_index_name = get_new_index_name
-          indexing_process
-
+          indexing_process(new_index_name)
           self.__elasticsearch__.client.indices.update_aliases body: {
             actions: [
               { add: { index: new_index_name, alias: self.index_name } }
@@ -69,8 +67,7 @@ module Palette
         def reindex!
           new_index_name = get_new_index_name
           old_index_name = get_old_index_name
-          indexing_process
-
+          indexing_process(new_index_name)
           self.__elasticsearch__.client.indices.update_aliases body: {
             actions: [
               { remove: { index: old_index_name, alias: self.index_name } },
@@ -90,7 +87,7 @@ module Palette
         after_destroy :delete_document
 
         def delete_document
-          self.__elasticsearch__.delete_document
+          self.__elasticsearch__.delete_document rescue nil
         end
 
         index_name { "#{Rails.env.downcase.underscore}_#{self.connection.current_database}_#{self.table_name.underscore}" }
