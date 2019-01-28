d592fe36ec02a924845cbe4dc9b1fa99b0a86011
Hiroyuki Nikaido <hiroyuki.nikaido@gmail.com>
2019-01-28 18:54:50 +0900

    add number_of_shards and number_of_replicas to configurations

--- lib/palette/elastic_search/searchable.rb
+++ lib/palette/elastic_search/searchable.rb
@@ -113,8 +113,8 @@ module Palette
 
         settings index:
                    {
-                     number_of_shards:   1,
-                     number_of_replicas: 0,
+                     number_of_shards: ::Palette::ElasticSearch.configuration.number_of_shards,
+                     number_of_replicas: ::Palette::ElasticSearch.configuration.number_of_replicas,
                      analysis: {
                        tokenizer: {
                          kuromoji_search: {
