392b6628f0d377b09f667bfa9c9ee9ea5f8a0f13
Hiroyuki Nikaido <hiroyuki.nikaido@gmail.com>
2019-01-28 19:01:32 +0900

    rm attr

--- lib/palette/elastic_search/configuration.rb
+++ lib/palette/elastic_search/configuration.rb
@@ -3,13 +3,9 @@ module Palette
     class Configuration
       include Singleton
       attr_accessor :run_callbacks
-      attr_accessor :number_of_shards
-      attr_accessor :number_of_replicas
 
       def initialize
         @run_callbacks = true
-        @number_of_shards = 1
-        @number_of_replicas = 0
       end
     end
   end
