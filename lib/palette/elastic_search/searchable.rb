c290a5f5d4c11f2893a79f0b01b507fe29297538
rei-akuto <akutoyuuki@gmail.com>
2019-03-26 16:17:58 +0900

    space_trimmer作成

--- lib/palette/elastic_search/searchable.rb
+++ lib/palette/elastic_search/searchable.rb
@@ -165,11 +165,11 @@ module Palette
                          },
                          bigram: {
                            tokenizer: 'bi_gram',
-                           char_filter: %W(my_icu_normalizer)
+                           char_filter: %W(my_icu_normalizer space_trimmer)
                          },
                          ngram: {
                            tokenizer: 'n_gram',
-                           char_filter: %W(my_icu_normalizer)
+                           char_filter: %W(my_icu_normalizer space_trimmer)
                          },
                          katakana: {
                            tokenizer: 'whitespace',
@@ -240,7 +240,12 @@ module Palette
                            type: 'pattern_replace',
                            pattern: '[\x{30FC}\x{2010}-\x{2015}\x{2212}\x{FF70}-]',
                            replacement: ''
-                         }
+                         },
+                         space_trimmer: {
+                          type: 'pattern_replace',
+                          pattern: '[\s]',
+                          replacement: ''
+                        }
                        }
                      }
                    }
